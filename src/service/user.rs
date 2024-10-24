fbc98169fde9944729875adba28aea894ad49cb7
zlqu <zlqu@trip.com>
2024-10-25 01:26:28 +1100

    feat: login

--- src/service/user.rs
+++ src/service/user.rs
@@ -1,18 +1,18 @@
 use crate::{
-    constant::AUTH_,
     model::user::{GetUserInfo, LoginInfo, UserInfo},
     util::{create_jwt, get_token_key},
 };
-use actix_web::{cookie::Cookie, get, post, web, HttpRequest, HttpResponse, Responder};
+use actix_web::{
+    cookie::{
+        time::{self, OffsetDateTime},
+        Cookie,
+    },
+    get, post, web, HttpResponse, Responder,
+};
 use sqlx::PgPool;
 
 #[post("login")]
-pub async fn login(
-    pool: web::Data<PgPool>,
-    login_info: web::Json<LoginInfo>,
-    req: HttpRequest,
-) -> impl Responder {
-    let path = req.path();
+pub async fn login(pool: web::Data<PgPool>, login_info: web::Json<LoginInfo>) -> impl Responder {
     let result = sqlx::query_as::<_, UserInfo>(
         "SELECT * 
         FROM retailer 
@@ -28,10 +28,10 @@ pub async fn login(
             let jwt: String = create_jwt(user_info.id, &user_info.email, &user_info.name);
             let token_key = get_token_key(login_info.p.clone());
             let cookie = Cookie::build(token_key, jwt)
-                .path(login_info.p.clone())
+                .path("/")
+                .max_age(time::Duration::days(1))
                 .finish();
 
-            println!("{}", cookie);
             HttpResponse::Ok().cookie(cookie).json(user_info)
         }
         Err(_) => HttpResponse::InternalServerError().finish(),
