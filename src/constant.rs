49faf5df0d3f4f4811d157f76a677f591d671dea
zlqu <zlqu@trip.com>
2024-10-26 16:16:07 +1100

    feat: apio

--- src/constant.rs
+++ src/constant.rs
@@ -1,6 +1,6 @@
 pub const EP_AUTH: &str = "ep_auth_token";
 pub const RETAILER_AUTH: &str = "retailer_auth_token";
 
-// pub const HOST: &str = "https://charlescrazy.fun/ai/";
+pub const HOST: &str = "https://charlescrazy.fun/ai/";
 
-pub const HOST: &str = "http://127.0.0.1:3002/ai/";
+// pub const HOST: &str = "http://127.0.0.1:3002/ai/";
